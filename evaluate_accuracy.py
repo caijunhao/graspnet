50632e623c714ddbf4033d214a729a7d9f10c9f8
caijunhao <caijh28@mail2.sysu.edu.cn>
2018-02-25 16:42:49 +0800

    add evaluate script

---
+++ evaluate_accuracy.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='evaluate grasping accuracy.')
+parser.add_argument('--file_path',
+                    default='/home/caijunhao/ros_ws/src/graspnet/semi_supervised_domain_adapt_753_result_3/grasping_label.txt',
+                    type=str,
+                    help='path to the file with grasping label.')
+args = parser.parse_args()
+
+
+def main():
+    with open(args.file_path, 'r') as f:
+        annotations = f.readlines()
+    num_sample = len(annotations)
+    num_success = 0
+    for annotation in annotations:
+        _, _, label = annotation.split(' ')
+        num_success += 1 - int(label)
+    print 'num_success: {}'.format(num_success)
+    print 'num_trials: {}'.format(num_sample)
+    print 'accuracy: {}'.format(num_success * 1.0 / num_sample)
+
+
+if __name__ == '__main__':
+    main()
+
